cd069c1e84c2563deed39e0c8affcec5c583a7c5
tony figueras <tonfy@example.com>
2024-09-21 10:04:32 -0400

    vercel

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -88,7 +88,39 @@ const bookController = {
         } catch (error) {
             res.json({msg: error.msg})
         }
-    }
+    },
+    login: async (req, res) => {
+        try {
+          res.json({msg: 'login exitoso' });
+          const { username, password } = req.body;
+    
+          // Buscar el usuario en la base de datos
+          const sql = 'SELECT * FROM users WHERE username = $1';
+          const { rows } = await postgre.query(sql, [username]);
+    
+          // Si no se encuentra el usuario
+          if (rows.length === 0) {
+            return res.status(401).json({ error: 'Credenciales incorrectas' });
+          }
+    
+          const user = rows[0];
+    
+          // Verificar la contraseña
+          const isMatch = await bcrypt.compare(password, user.password);
+          if (!isMatch) {
+            return res.status(401).json({ error: 'Credenciales incorrectas' });
+          }
+    
+          // Generar el token JWT
+          const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    
+          // Enviar el token como respuesta
+          res.json({msg: 'login exitoso',  token });
+        } catch (error) {
+          console.error('Error en el login:', error);
+          res.status(500).json({ msg: 'Error en el login', error: error.message });
+        }
+      },
 }
 
 module.exports = bookController
